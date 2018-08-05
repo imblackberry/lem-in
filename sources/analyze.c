1faf62eadb93a0308562a4ee097ce7cc148789a1
imblackbarry <blokhavictoria@gmail.com>
2018-08-06 01:51:49 +0300

    formula done

--- sources/analyze.c
+++ sources/analyze.c
@@ -60,18 +60,22 @@ int		search_and_add_way(t_farm *farm, int *room_way_now, int way_length_now)
 int top_ways(t_farm *farm)
 {
 	farm->top = new_top();
-	farm->cur_top = ft_memalloc((sizeof(t_way*) * farm->n_all_ways + 1));
-	search_top(farm, farm->all_ways, 0, 0);
-	if (farm->top->steps == 0)
-	{
-		free(farm->cur_top);
-		return (-1);
-	}
-
-	free(farm->cur_top);
-	ft_printf("___________TOP____________\n");
-	show_top(farm->top);
-	ft_printf("_______________________________________\n");
+    if (farm->all_ways->length == 1)
+		update_top(farm->top, farm->cur_top, farm->cur_n_ids, 1);
+    else
+    {
+        farm->cur_top = ft_memalloc((sizeof(t_way*) * farm->n_all_ways + 1));
+        search_top(farm, farm->all_ways, 0, 0);
+        if (farm->top->steps == 0)
+        {
+            free(farm->cur_top);
+            return (-1);
+        }
+        free(farm->cur_top);
+        ft_printf("___________TOP____________\n");
+        show_top(farm->top);
+        ft_printf("_______________________________________\n");
+    }
 	return (1);
 }
 
@@ -82,31 +86,23 @@ int	search_top(t_farm *farm,  t_way *way_now, int ants_come, int cur_steps)
 				
 		if (no_last_way_intersec(farm->cur_top, farm->cur_n_ids, way_now) == 1)
 		{
-			ft_printf("****************************\n");
-			
-					// ft_printf("ANTS_COME = %d\n", ants_come);
-				show_way_arr(farm->cur_top, farm->cur_n_ids);
-				ft_printf("WAY_NOW\nid = %d \n", way_now->id);
-				show_room_way(way_now->room_way, way_now->length);
-				ft_printf("*****************************\n");
+                    ft_printf("****************************\n");
+                    show_way_arr(farm->cur_top, farm->cur_n_ids);
+                    ft_printf("WAY_NOW\nid = %d \n", way_now->id);
+                    show_room_way(way_now->room_way, way_now->length);
+                    ft_printf("*****************************\n");
 			farm->cur_top[farm->cur_n_ids] = way_now;
 			farm->cur_n_ids++;
-			if (way_now->length == 1)
-			{
-				cur_steps = 1;
-				update_top(farm->top, farm->cur_top, farm->cur_n_ids, cur_steps);
-				return (0);
-			}
-			ft_printf(" ANTS_COME = %d\n", ants_come);
+			        ft_printf(" ANTS_COME = %d\n", ants_come);
 			ants_come = change_ants_come(ants_come, farm->cur_top, farm->cur_n_ids, way_now);
 			cur_steps = way_now->length - 1 + count_steps(ants_come, farm->ants, farm->cur_n_ids);
-			ft_printf("STEPS = %d\n", cur_steps);
+		        	ft_printf("STEPS = %d\n", cur_steps);
 			if (farm->top->steps == 0 || cur_steps < farm->top->steps)
 			{
 				update_top(farm->top, farm->cur_top, farm->cur_n_ids, cur_steps);
-					ft_printf("___________TOP____________\n");
-				show_top(farm->top);
-				ft_printf("_______________________________________\n");
+                        ft_printf("___________TOP____________\n");
+                        show_top(farm->top);
+                        ft_printf("_______________________________________\n");
 			}
 			search_top(farm, way_now->next, ants_come, cur_steps);
 			farm->cur_top[farm->cur_n_ids] = NULL;
@@ -119,14 +115,12 @@ int	search_top(t_farm *farm,  t_way *way_now, int ants_come, int cur_steps)
 
 int	change_ants_come(int ants_come, t_way **cur_top, int cur_n_ids, t_way *way_now)
 {
-	ft_printf(" IN FUNCTION ANTS_COME = \t\t\t\t%d ", ants_come);
+	// ft_printf("\t\t\t\tIN FUNCTION ANTS_COME = %d ", ants_come);
 	if (cur_n_ids <= 1)
 		return (0);
-	if ( cur_top[cur_n_ids - 1]->length - 1 - cur_top[cur_n_ids - 2]->length < 0)
-		return (ants_come);
-	ants_come = ants_come + (cur_top[cur_n_ids - 1]->length - 1 - cur_top[cur_n_ids - 2]->length) * cur_n_ids ;
-	ft_printf(" + (%d - 1 - %d) * %d = %d\n", cur_top[cur_n_ids - 1]->length, cur_top[cur_n_ids - 2]->length, cur_n_ids,  ants_come);
-	ft_printf(" IN FUNCTION ANTS_COME = \t\t\t\t%d ", ants_come);
+	ants_come = ants_come + (cur_top[cur_n_ids - 1]->length - cur_top[cur_n_ids - 2]->length) * (cur_n_ids - 1) ;
+	// ft_printf(" + (%d - %d) * %d = %d\n", cur_top[cur_n_ids - 1]->length, cur_top[cur_n_ids - 2]->length, cur_n_ids - 1,  ants_come);
+	// ft_printf("\t\t\t\tIN FUNCTION ANTS_COME = %d\n", ants_come);
 	way_now = cur_top[cur_n_ids - 1];
 	return (ants_come);
 }
@@ -141,7 +135,11 @@ int count_steps(int ants_come, int ants, int cur_n_ids)
 	ants_come = (ants - ants_come);
 	steps =  ants_come / cur_n_ids;
 	if  (ants_come % cur_n_ids > 0)
+    {
+        // ft_printf("\t\t\t\tIN FUNCTION STEPS + 1");
 		steps++;
+    }
+    // ft_printf("\t\t\t\tIN FUNCTION STEPS = %d / %d =%d ", ants_come, cur_n_ids, steps);
 	return (steps);
 }
 
@@ -149,27 +147,18 @@ int count_steps(int ants_come, int ants, int cur_n_ids)
 // 7
 // 10
 // 1234567
-// ----
-// ----
-//  ----
-//  ----
-//   ----
-//   ----
-//    ----
-//    ----
+// ---
+// ---
+//  ---
+//  ---
+//   ---
+//   ---
+//    ---
+//    ---
+
+//     ---
+//     ---
 // -------
-//     ----
-//  -------
-//      ----
-//   -------
-
-
-
-//       ----
-//    -------
-// ----------
-
-
 
 // 1234567
 // ----
